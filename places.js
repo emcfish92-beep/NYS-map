b3f6f3fef76c2d2d848e0b98e069b8781a392070
Emily Fisher <emilyfisher@dhcp-10-83-133-144.wireless.buffalo.edu>
2026-08-24 12:50:43 -0400

    Refresh PWA cache

---
+++ places.js
@@ -0,0 +1,226 @@
+export const places = [
+  {
+    name: "Letchworth SP",
+    category: "State_Park",
+    visited: true,
+    coordinates: [-77.98333, 42.63472],
+    description: "NYS Park, -77.98333, 42.63472, Been."
+  },
+  {
+    name: "Franklin Gulf",
+    category: "Erie_County_Park",
+    visited: true,
+    coordinates: [-78.9394795, 42.6056156],
+    description: "Erie County Park, -78.9394795, 42.6056156, Been."
+  },
+  {
+    name: "Harlem Road",
+    category: "Kayak_launch",
+    visited: false,
+    coordinates: [-78.79075, 42.86751],
+    description: "Kayak launch, -78.79075, 42.86751."
+  },
+  {
+    name: "Amherst Veterans Boat Ramp",
+    category: "Kayak_launch",
+    visited: false,
+    coordinates: [-78.80166, 43.06455],
+    description: "Kayak launch, -78.80166, 43.06455."
+  },
+  {
+    name: "Wilkeson Point Kayak Launch",
+    category: "Kayak_launch",
+    visited: false,
+    coordinates: [-78.88216, 42.87074],
+    description: "Kayak launch, -78.88216, 42.87074."
+  },
+  {
+    name: "Woods Creek Kayak Launch",
+    category: "Kayak_launch",
+    visited: false,
+    coordinates: [-78.971774, 43.057857],
+    description: "Kayak launch, -78.971774, 43.057857."
+  },
+  {
+    name: "Eagle Overlook Kayak Launch",
+    category: "Kayak_launch",
+    visited: false,
+    coordinates: [-78.997752, 43.052848],
+    description: "Kayak launch, -78.997752, 43.052848."
+  },
+  {
+    name: "Griffon Park Kayak Launch",
+    category: "Kayak_launch",
+    visited: false,
+    coordinates: [-78.952589, 43.074641],
+    description: "Kayak launch, -78.952589, 43.074641."
+  },
+  {
+    name: "Whitehaven Overlook Kayak Launch",
+    category: "Kayak_launch",
+    visited: false,
+    coordinates: [-79.020511, 43.017213],
+    description: "Kayak launch, -79.020511, 43.017213."
+  },
+  {
+    name: "Fix Road Overlook Kayak Launch",
+    category: "Kayak_launch",
+    visited: false,
+    coordinates: [-79.002696, 42.982518],
+    description: "Kayak launch, -79.002696, 42.982518."
+  },
+  {
+    name: "West Canal Park and Marina Kayak Launch",
+    category: "Kayak_launch",
+    visited: false,
+    coordinates: [-78.8218, 43.0520],
+    description: "Kayak launch, -78.8218, 43.0520."
+  },
+  {
+    name: "Amherst Veteran's Boat Ramp",
+    category: "Kayak_launch",
+    visited: false,
+    coordinates: [-78.8026, 43.0651],
+    description: "Kayak launch, -78.8026, 43.0651."
+  },
+  {
+    name: "Beaver Island SP Kayak Launch",
+    category: "Kayak Launch",
+    visited: true,
+    coordinates: [-78.956336, 42.959553],
+    description: "Kayak Launch, -78.956336, 42.959553."
+  },
+  {
+    name: "Beaver Island SP",
+    category: "State_Park",
+    visited: true,
+    coordinates: [-78.9562, 42.9606],
+    description: "NYS Park, -78.9562, 42.9606, Been."
+  },
+  {
+    name: "Mutual Riverfront Park Kayak Launch",
+    category: "Kayak_launch",
+    visited: true,
+    coordinates: [-78.86004, 42.86452],
+    description: "Kayak launch, -78.86004, 42.86452, Been."
+  },
+  {
+    name: "Ohio Street Kayak Launch",
+    category: "Kayak_launch",
+    visited: false,
+    coordinates: [-78.86838, 42.86576],
+    description: "Kayak launch, -78.86838, 42.86576."
+  },
+  {
+    name: "Eastern Park Kayak Launch",
+    category: "Kayak_launch",
+    visited: true,
+    coordinates: [-78.8679, 43.0146],
+    description: "Kayak launch, -78.8679, 43.0146, Been."
+  },
+  {
+    name: "Niagara and Sweeney Streets Kayak Launch",
+    category: "Kayak_launch",
+    visited: false,
+    coordinates: [-78.86307, 43.02348],
+    description: "Kayak launch, -78.86307, 43.02348."
+  },
+  {
+    name: "Ellicot Creek Kayak Launch",
+    category: "Kayak_launch",
+    visited: false,
+    coordinates: [-78.823700, 43.023970],
+    description: "Kayak launch, -78.823700, 43.023970."
+  },
+  {
+    name: "North Tonawanda Kayak Launch",
+    category: "Kayak_launch",
+    visited: false,
+    coordinates: [-78.827359, 43.038304],
+    description: "Kayak launch, 78.827359, 43.038304."
+  },
+  {
+    name: "Flume Falls",
+    category: "Waterfall",
+    visited: false,
+    coordinates: [-73.84101, 44.366448],
+    description: "Waterfall, -73.84101, 44.366448."
+  },
+  {
+    name: "Bash Bish Falls",
+    category: "Waterfall",
+    visited: false,
+    coordinates: [-73.4928, 42.1147],
+    description: "Waterfall, -73.4928, 42.1147."
+  },
+  {
+    name: "OK Slip Falls",
+    category: "Waterfall",
+    visited: false,
+    coordinates: [-74.129670, 43.772470],
+    description: "Waterfall, -74.129670, 43.772470."
+  },
+  {
+    name: "Flume Falls",
+    category: "Waterfall",
+    visited: false,
+    coordinates: [-73.84101, 44.366448],
+    description: "Waterfall, -73.84101, 44.366448."
+  },
+  {
+    name: "Flume Falls",
+    category: "Waterfall",
+    visited: false,
+    coordinates: [-73.84101, 44.366448],
+    description: "Waterfall, -73.84101, 44.366448."
+  },
+  {
+    name: "BearTown State Park",
+    category: "State_Park",
+    visited: true,
+    coordinates: [-80.2753, 38.0523],
+    description: "State Park, -80.2753, 38.0523."
+  },
+  {
+    name: "Cuyahoga National Park",
+    category: "National_Park",
+    visited: true,
+    coordinates: [-81.5497, 41.2417],
+    description: "National Park, -81.5497, 41.2417."
+  },
+  {
+    name: "Indiana Dunes National Park",
+    category: "National_Park",
+    visited: false,
+    coordinates: [-87.008989, 41.677434],
+    description: "National Park, -87.008989, 41.677434."
+  },
+  {
+    name: "Mammoth Cave National Park",
+    category: "National_Park",
+    visited: true,
+    coordinates: [-86.159943, 37.183640],
+    description: "National Park, -86.159943, 37.183640."
+  },
+  {
+    name: "New River Gorge National Park",
+    category: "National_Park",
+    visited: true,
+    coordinates: [-81.0761, 38.0705],
+    description: "National Park, -81.0761, 38.0705."
+  },
+  {
+    name: "Smoky Mountains National Park",
+    category: "National_Park",
+    visited: true,
+    coordinates: [-83.489548, 35.611763],
+    description: "National Park, -83.489548, 35.611763."
+  },
+  {
+    name: "Yosemite National Park",
+    category: "National_Park",
+    visited: true,
+    coordinates: [-119.5383, 37.8651],
+    description: "National Park, -119.5383, 37.8651."
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "outdoor-map-v1";
+const CACHE_NAME = "outdoor-map-v2";
 
 const APP_FILES = [
   "./",
