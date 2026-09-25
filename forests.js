92bf2e03b0f0fd4c674f19965f9492bc8b1a9b90
Emily Fisher <emilyfisher@dhcp-10-83-129-9.wireless.buffalo.edu>
2026-09-25 15:22:23 -0400

    national forest shading

---
+++ forests.js
@@ -0,0 +1,10 @@
+export const forests = [
+
+  {
+    id: "allegheny-national-forest",
+    name: "Allegheny National Forest",
+    file: "forests/allegheny-national-forest.geojson",
+    color: "#8b5cf6"
+  }
+
+];
